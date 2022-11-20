bccf95f716ae9f29b9b2c959a7861024a73a680b
nayproject <115395427+nayganteng@users.noreply.github.com>
2022-11-20 16:59:46 +0700

    Start Shell✨

---
+++ start.sh
@@ -0,0 +1,88 @@
+clear
+echo "ᴍᴀꜱᴜᴋ ɢᴀᴍᴇ ᴅᴀʜᴜʟᴜ"
+sleep 5
+clear
+
+start=`date +%s`
+echo "ғᴇᴛᴄʜᴛɪɴɢ ᴅᴇᴠɪᴄᴇ ɪɴғᴏ :
+"
+sleep 1
+echo "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+sleep 2
+echo -n "ᴅᴀᴛᴇ & ᴛɪᴍᴇ: "
+date "+%a %d %b %Y %Z %H:%M:%S"
+echo "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+sleep 1
+echo "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo -n "ᴢᴏɴᴇ: "
+getprop persist.sys.timezone
+echo "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+sleep 1
+echo "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo -n "ɢsᴍ ᴏᴘᴇʀᴀᴛᴏʀ: "
+getprop gsm.operator.alpha
+echo "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+sleep 1
+echo "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo -n "ɪsᴏ ᴄᴏᴜɴᴛʀʏ: "
+getprop gsm.operator.iso-country
+echo "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+sleep 1
+echo "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo -n "ᴅᴇᴠɪᴄᴇ ʙʀᴀɴᴅ: "
+getprop ro.product.brand
+sleep 1
+echo "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+sleep 1
+echo "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo -n "ᴅᴇᴠɪᴄᴇ ᴍᴏᴅᴇʟ: "
+getprop ro.product.model
+echo "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+sleep 1
+printf " ";
+sleep 1
+printf " ";
+sleep 1
+echo " "
+clear
+echo "✪︎︎︎ ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ..... ✪︎︎︎";
+sleep 5
+echo
+echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█";
+echo
+echo "═╉━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "═╉➥ ✨ ɢɪᴛʜᴜʙ : ɴᴀʏɢᴀɴᴛᴇɴɢ "
+sleep 1
+echo "═╉━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "═╉➥ ✨ ʙʏ : @ɴᴀᴍᴀᴋᴏᴋᴡɪʙᴜ"
+sleep 1
+echo "═╉━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "═╉➥ ✨ ᴄʜᴇᴀᴛ ᴠᴇʀsɪᴏɴ : v1"
+sleep 1
+echo "═╉━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo
+sleep 2
+echo "🌟 ᴀᴄᴛɪᴠᴇ ᴠᴀʟᴜᴇꜱ ɢᴀᴍᴇ : ᴄᴏᴅᴍ 🌟"
+echo
+sleep 1
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo -n "0xa5cb8" ; sleep .5 ; echo -n "0xa6fe4" ; sleep .5 ; echo -n "0xa78cc" ; sleep .5 ; echo -n "0xa7984"; 
+echo
+chmod 777 /data/data/com.termux/files/home/codm/base.so
+su -c /data/data/com.termux/files/home/codm/base.so
+sleep 3
+echo " "
+echo "🌟..................................................🌟"
+       
